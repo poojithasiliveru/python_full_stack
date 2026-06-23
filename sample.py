9cb784cccb85985564db840b5ec9158537b9ccb9
poojithasiliveru <poojithasiliveru2003@gmail.com>
2026-06-23 19:07:23 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,36 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print(8+7)
+15
+>>> print(9-7)
+2
+>>> print(7*6)
+42
+>>> print("Codegnan")
+Codegnan
+>>> print('Python')
+Python
+>>> print('''Vijayawada''')
+Vijayawada
+>>> 
+========= RESTART: C:/Users/poojitha/Downloads/python_full_stack/add.py ========
+15
+>>> 
+========= RESTART: C:/Users/poojitha/Downloads/python_full_stack/sub.py ========
+5
+>>> 
+========= RESTART: C:/Users/poojitha/Downloads/python_full_stack/mul.py ========
+54
+>>> 
+======== RESTART: C:/Users/poojitha/Downloads/python_full_stack/first.py =======
+Hello World!
+>>> 
+======= RESTART: C:/Users/poojitha/Downloads/python_full_stack/second.py =======
+Python_full_stack
+>>> 
+======== RESTART: C:/Users/poojitha/Downloads/python_full_stack/third.py =======
+13
+3
+28
+Vijayawada
+hello
