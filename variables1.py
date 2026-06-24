b0aa4e72f87ddd41e052133f58558dfb53d57121
poojithasiliveru <poojithasiliveru2003@gmail.com>
2026-06-24 14:31:32 +0530

    Add files via upload

---
+++ variables1.py
@@ -0,0 +1,82 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#Variables
+a=10
+print(a)
+10
+b=2
+c=6
+print(b+c)
+8
+x=20
+print(x)
+20
+y=70
+print(y)
+70
+z=90
+print(z)
+90
+a7=67
+print(a7)
+67
+c0978652=1000
+print(c0978652)
+1000
+q0987654321=9000
+print(q0987654321)
+9000
+name="Poojitha"
+print(name)
+Poojitha
+city="Vijayawada"
+print(city)
+Vijayawada
+country="India"
+print(country)
+India
+@c=40
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+_c=50
+print(_c)
+50
+>>> _p=87
+>>> print(_p)
+87
+>>> mailid="poojithasiliveru2003@gmail.com"
+>>> print(mailid)
+poojithasiliveru2003@gmail.com
+>>> #it doesn't allow keywords
+>>> if=90
+SyntaxError: invalid syntax
+>>> elif=89
+SyntaxError: invalid syntax
+>>> print=10
+>>> print(print)
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    print(print)
+TypeError: 'int' object is not callable
+>>> break=67
+SyntaxError: invalid syntax
+>>> #multiple values in a single line
+>>> a=4,b=9
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a=4;b=9
+>>> print(a+b)
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    print(a+b)
+TypeError: 'int' object is not callable
+>>> a1=5;b1=9
+>>> print(a1+b1)
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    print(a1+b1)
+TypeError: 'int' object is not callable
+>>> g=5;f=9
+>>> print(g+f)
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    print(g+f)
+TypeError: 'int' object is not callable
