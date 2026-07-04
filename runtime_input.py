0f7523ef04917b9a9ae35a6085dfdac5fa983812
poojithasiliveru <poojithasiliveru2003@gmail.com>
2026-07-04 22:26:04 +0530

    Add files via upload

---
+++ runtime_input.py
@@ -0,0 +1,85 @@
+#print(3+4)
+
+'''a=10
+b=20
+print(a+b)'''
+
+'''a=4
+b=9
+c=a+b
+print(c)
+
+#run time input()
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+print(a+b) 
+
+a=float(input("Enter a value:"))
+b=float(input("Enter b value:"))
+print(a+b)
+
+a=str(input("Data1:"))
+b=str(input("Data2:"))
+print(a+b)
+
+a=input("Data1:")
+b=input("Data2:")
+print(a+b)
+
+fname=input("fname:")
+lname=input("lname:")
+print((fname+ " " +lname).title())
+
+a=complex(input("Enter a value:"))
+b=complex(input("Enter b value:"))
+print(a+b)
+
+a=bool(input("Data1:"))
+b=bool(input("Data2:"))
+print(a+b)
+
+#it will print how many runtimes we have taken
+a=bool(input("Data1:"))
+b=bool(input("Data2:"))
+c=bool(input("Data3:"))
+print(a+b+c)'''
+
+'''a=int(input("a value:"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value:"))
+b=int(input("b value"))
+option=int(input("choose the option
+                 1.add
+                 2.sub
+                 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+#By taking str input we can give integer value and string data
+'''a=int(input("a value:"))
+b=int(input("b value"))
+option=(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+#without giving script in the input function
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+a=int(input())
+print(a)
+
+
+
+
+
+
+      
